94b0e9efa3959c9d059a9b031f8dea5fd2416eed
Pat Paternostro <ppaternostro@yahoo.com>
2026-01-27 12:22:57 -0500

    Add header component to application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddressComponent } from './component/address/address.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddressComponent],
+  imports: [RouterOutlet, AddressComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
